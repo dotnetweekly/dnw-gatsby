c1cbaf75b5db20bfd97826e51f2a42ed36a79a90
Bill Stavroulakis <bill.stavroulakis@getcruise.com>
2019-01-24 23:28:02 -0800

    update feed

--- scripts/copy-static.js
+++ scripts/copy-static.js
@@ -4,7 +4,7 @@ const fse = require('fs-extra')
 
 fs.readdir(path.resolve(__dirname, '../src/static'), (err, files) => {
   files.forEach(file => {
-    if (file.includes('.') !== -1) {
+    if (file.includes('.') !== -1 && file !== 'feed') {
       fs.copyFile(
         `${path.resolve(__dirname, '../src/static')}/${file}`,
         `${path.resolve(__dirname, '../public')}/${file}`,
